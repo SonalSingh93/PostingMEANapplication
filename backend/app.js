31d60994a63853743794c91bda808f844d4f4a2e
Sonal Singh <singh.son@husky.neu.edu>
2018-11-11 23:06:27 -0500

    image in frontend completed:

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use("/images", express.static(path.join('backend/images')));
 
 mongoose.connect("mongodb+srv://sonal:yymI5Mhr9ECXRWrM@cluster0-opklo.mongodb.net/node-angular?retryWrites=true")
 .then(() => {
